4e1c8c2072fe188acbcd0f7bb1246b139796f5cc
Erik Barriga <erik.barriga@hotmail.com>
2019-01-23 15:33:10 -0600

    First Realease

--- ToolBackupDataBaseNet/Mail/Email.cs
+++ ToolBackupDataBaseNet/Mail/Email.cs
@@ -5,7 +5,6 @@ using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using ToolBackupDataBaseNet.Mail.Model;
-
 namespace ToolBackupDataBaseNet.Mail
 {
     class Email:IDisposable     
